502e76da6a035eee04b7f0a15e72416c78873bef
LU Hua <yukiluq@gmail.com>
2017-10-08 11:25:13 +0800

    private
    private

--- typescript/interface.ts
+++ typescript/interface.ts
@@ -1,4 +1,4 @@
-/* interface的声明类型、
+/* interface的声明类型
  * 参考链接： https://www.tslang.cn/docs/handbook/interfaces.html
  * 1. 字面量对象，非new出来的对象，(new的用class直接声明较好)
  * 2. 函数
@@ -12,6 +12,15 @@
  * 
  * 2. 写明属性值，表示对象下的属性及方法，例如表示字面量对象类型
  * interface o { a: number， method(arg: number): string }
+ * 
+ * 
+ * interface的继承
+ * 与C#或Java里接口的基本作用一样，TypeScript也能够用它来明确的强制一个类去符合某种契约
+ * 1. 但与java中不同的是，java中的接口是不能有实例域的，即属性，只能有未实现的方法，而Typescript中是可以有实例域的
+ * Typescript中当类实现接口时，除了要实现接口中的方法细节，实例域(属性)也要一起写出来
+ * 2. 接口描述了类的公共部分，而不是公共和私有两部分。 它不会帮你检查类是否具有某些私有成员。
+ * 3. 当一个类实现了一个接口时，只对其实例部分进行类型检查，静态方法比如constructor不在检查范围内
+ * 4. 接口可以继承类
  */
 
  // 字面量对象
@@ -69,3 +78,81 @@ function getCounter(): Counter {
 
 let c = getCounter();
 console.log(c(10), c.interval, c.reset());
+
+
+// 类实现接口
+
+// 1. 如上所述，java中接口不能包含实例域，ts中却可以，只是在类实现接口时，要将实例域(属性)一起写出
+// 2. 接口只描述类的公共部分，所以在接口中并不存在private, protected, public修饰符，这些只在类中存在
+interface MyInterface {
+    a: number;
+    getA(): number;
+}
+
+class MyClass implements MyInterface {
+    a: number;
+    getA(): number {
+        return this.a;
+    }
+}
+
+// 接口继承类
+
+// 不继承具体实现，只继承类成员
+class MyControl {
+    public state: any;
+    f(): number { return 0; }  // 默认为public
+}
+
+interface MySelectableControl extends MyControl {
+    select(): void;
+}
+
+/* 接口MySelectableControl包含了MyControl中的类成员，相当于如下
+ * interface MySelectableControl {
+ *   public state: any;
+ *   f(): number;
+ *   select(): void;
+ * }
+ */
+
+class MyImageA implements MySelectableControl { // 将上述包含三个成员的接口实现
+    public state: any;
+    select() { }
+    f(): number { return 1; }
+}
+
+// 但是若类MyControl中的成员为私有，根据私有成员兼容性，情况就不同了
+
+// 私有成员兼容性
+// 成员公有时，不写访问权限时，默认public，ts只检查对应类型，而不是看类名，类型相同即可，不需要类名也相同
+class PublicA { a: number }
+class PublicB { a: number }
+let pa: PublicA = new PublicA;
+let pb: PublicB = new PublicB;
+pa = pb;  // 类型相同，可以互通
+
+// 当存在私有成员时，即使类型相同，也认为不同类之间私有成员时不同的
+class PrivateA { private a: number; }
+class PrivateB { private b: number; }
+let pra: PrivateA = new PrivateA;
+let prb: PrivateB = new PrivateB;
+pra = prb;  // 错误，类型不同，不能互通
+
+// 所以当接口继承的类含有私有成员时，只有被继承类的子类才能实现该接口，由于私有成员的兼容性问题，其他类是无法实现接口被继承类的私有成员的
+class Control {
+    private state: any;
+}
+
+interface SelectableControl extends Control { // 继承了类Control的私有成员state
+    select(): void;
+}
+
+class Button extends Control implements SelectableControl { // Button是Control的子类，含有Control的私有成员state，可以实现上述接口
+    select() { }
+}
+
+class MyImage implements SelectableControl { // 错误，不能实现上述接口
+    private state: any;  // 私有成员兼容性问题，无法该state与Control类中的state不同
+    select() { }
+}
